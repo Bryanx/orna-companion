3542e4c222152281fba2fde203873593dff672dd
Bryan de Ridder <br.deridder@gmail.com>
2021-02-28 13:25:55 +0100

    Add loading symbol

--- app/src/main/java/nl/bryanderidder/ornaguide/shared/bindingadapter/RecyclerViewBinding.kt
+++ app/src/main/java/nl/bryanderidder/ornaguide/shared/bindingadapter/RecyclerViewBinding.kt
@@ -1,5 +1,6 @@
 package nl.bryanderidder.ornaguide.shared.bindingadapter
 
+import android.view.View
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import nl.bryanderidder.ornaguide.characterclass.model.CharacterClass
@@ -11,5 +12,15 @@ object RecyclerViewBinding {
   @BindingAdapter("adapterCharacterClassList")
   fun bindAdapterCharacterClassList(view: RecyclerView, items: List<CharacterClass>?) {
     (view.adapter as CharacterClassAdapter).setItemList(items ?: listOf())
+  }
+
+  @JvmStatic
+  @BindingAdapter("gone")
+  fun bindGone(view: View, shouldBeGone: Boolean) {
+    view.visibility = if (shouldBeGone) {
+      View.GONE
+    } else {
+      View.VISIBLE
+    }
   }
 }
