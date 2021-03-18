2af1fc3780e49d3d2da1bb9c80eaa814c2205146
Bryan de Ridder <br.deridder@gmail.com>
2021-03-18 09:11:07 +0100

    Refactor itemfilter

--- app/src/main/java/nl/bryanderidder/ornaguide/item/ui/list/filter/ItemFilter.kt
+++ app/src/main/java/nl/bryanderidder/ornaguide/item/ui/list/filter/ItemFilter.kt
@@ -8,21 +8,21 @@ import nl.bryanderidder.ornaguide.item.model.Item
  * @author Bryan de Ridder
  */
 data class ItemFilter(
-    var tiers: List<Int>? = listOf(),
-    var types: List<String>? = listOf(),
-    var elements: List<String>? = listOf(),
-    var equippedByList: List<String>? = listOf()
+    var tiers: List<Int> = listOf(),
+    var types: List<String> = listOf(),
+    var elements: List<String> = listOf(),
+    var equippedByList: List<String> = listOf()
 ) {
     fun applyFilter(list: List<Item>): List<Item> {
         var newList = list
-        if (tiers?.isNotEmpty() == true)
-            newList = newList.filter { item -> tiers?.contains(item.tier) == true }
-        if (types?.isNotEmpty() == true)
-            newList = newList.filter { item -> types?.contains(item.type) == true }
-        if (elements?.isNotEmpty() == true)
-            newList = newList.filter { item -> elements?.contains(item.element) == true }
-        if (equippedByList?.isNotEmpty() == true)
-            newList = newList.filter { item -> equippedByList?.any(item.equippedBy.toString()::contains) == true }
+        if (tiers.isNotEmpty())
+            newList = newList.filter { item -> tiers.contains(item.tier) }
+        if (types.isNotEmpty())
+            newList = newList.filter { item -> types.contains(item.type) }
+        if (elements.isNotEmpty())
+            newList = newList.filter { item -> elements.contains(item.element) }
+        if (equippedByList.isNotEmpty())
+            newList = newList.filter { item -> equippedByList.any(item.equippedBy.toString()::contains) }
         return newList
     }
 }
