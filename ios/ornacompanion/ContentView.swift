2eccce22d4ddace91eb44edc22311ed982fb8a7a
Bryan de Ridder <br.deridder@gmail.com>
2021-04-02 14:57:12 +0200

    Add ios

---
+++ ios/ornacompanion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ornacompanion
+//
+//  Created by Bryan de Ridder on 02/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/ornacompanion/ornacompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ornacompanionApp.swift
+//  ornacompanion
+//
+//  Created by Bryan de Ridder on 02/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ornacompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
