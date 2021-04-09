1a92a4c027f047b3265a57062468d39baf7e1ee4
Bryan de Ridder <br.deridder@gmail.com>
2021-04-10 00:09:50 +0200

    iOS: Add dark statusbar

--- ios/ornacompanion/ornacompanionApp.swift
+++ ios/ornacompanion/ornacompanionApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct ornacompanionApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DarkStatusBar {
+                ContentView()
+            }
         }
     }
 }
