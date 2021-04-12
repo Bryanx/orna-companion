3b4c4458c6b7631b752d4cb94dd66d2ece8357f5
Bryan de Ridder <br.deridder@gmail.com>
2021-04-12 11:53:33 +0200

    Bump version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId("nl.bryanderidder.ornaguide")
         minSdkVersion(26)
         targetSdkVersion(30)
-        versionCode(7)
-        versionName("1.6")
+        versionCode(8)
+        versionName("1.7")
 
         testInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
     }
