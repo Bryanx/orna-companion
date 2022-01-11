4dbb457e1e31d8b6fdad809f6d1e31c5be66b036
Bryan de Ridder <br.deridder@gmail.com>
2022-01-11 15:23:45 +0100

    Allow assessing items with not all fields filled

--- android/app/src/main/java/nl/bryanderidder/ornaguide/item/ui/detail/assess/ItemAssessViewModel.kt
+++ android/app/src/main/java/nl/bryanderidder/ornaguide/item/ui/detail/assess/ItemAssessViewModel.kt
@@ -72,26 +72,10 @@ class ItemAssessViewModel(
         view.hideKeyboard()
         return viewModelScope.launch {
             itemAssessBody.id = id
-            if (!assessBodyIsValid(stats))
-                toastMessage = "Please fill in all your item's details"
-            else
-                repository.assessItem(
-                    body = itemAssessBody,
-                    onError = { toastMessage = it })
-                    .collect(itemAssessResult::postValue)
+            repository.assessItem(
+                body = itemAssessBody,
+                onError = { toastMessage = it })
+                .collect(itemAssessResult::postValue)
         }
-    }
-
-    private fun assessBodyIsValid(stats: Map<String, Int>): Boolean = when {
-        stats.containsKey("Att") && itemAssessBody.attack == null -> false
-        stats.containsKey("Def") && itemAssessBody.defense == null -> false
-        stats.containsKey("HP") && itemAssessBody.hp == null -> false
-        stats.containsKey("Res") && itemAssessBody.resistance == null -> false
-        stats.containsKey("Mag") && itemAssessBody.magic == null -> false
-        stats.containsKey("Mana") && itemAssessBody.mana == null -> false
-        stats.containsKey("Dex") && itemAssessBody.dexterity == null -> false
-        stats.containsKey("Ward") && itemAssessBody.ward == null -> false
-        stats.containsKey("Crit") && itemAssessBody.crit == null -> false
-        else -> true
     }
 }
